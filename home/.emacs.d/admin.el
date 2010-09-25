7f8b746a44ad6105fa3f3292f4ba039e22677d1d
Scott Francis <scott@kurokoproject.com>
2010-09-24 17:31:47 -0700

    Add work-specific customizations for study

---
+++ home/.emacs.d/admin.el
@@ -0,0 +1,13 @@
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(setq org-agentda-files (list "~/org/work.org"))
+
+;; look-n-feel
+(set-face-attribute 'default nil :font "Inconsolata-dz-13")
+(color-theme-blackboard)
+(add-to-list 'default-frame-alist '(height . 50))
+(add-to-list 'default-frame-alist '(width . 190))
