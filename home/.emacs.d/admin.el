5703c5f7cf7b7d34cfd70452df92ea6d773ac077
Scott Francis <scott@kurokoproject.com>
2010-09-28 15:54:54 -0700

    Auto-commit from update_homesick_emacs

--- home/.emacs.d/admin.el
+++ home/.emacs.d/admin.el
@@ -2,12 +2,24 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+
+(global-set-key (kbd "<f12>") 'org-agenda)
+
 (setq org-log-done t)
 
-(setq org-agentda-files (list "~/org/work.org"))
+(setq org-agentda-files (list "/Users/admin/Documents/org/notes_2010.org"
+                              "/Users/admin/Documents/org/depot_central.org"
+                              "/Users/admin/Documents/org/refile.org"
+                              "/Users/admin/Documents/org/ub.org"
+                              "/Users/admin/Documents/org/yhr.org"))
 ;; FIXME: not sure how to add this path to emacs load path, since this
 ;; is a MacPorts-ism
 (setq-default ispell-program-name "/opt/local/bin/aspell")
+
+;; Vimpulse setup
+;; FIXME: not sure if this is the right way: unlike Vim the first j will be captured
+;; instead of echoed.
+;; (vimpulse-imap (kbd "jj") vimpulse-exit-insert-state)
 
 ;; look-n-feel
 (set-face-attribute 'default nil :font "Inconsolata-dz-13")
