ba70c7070530d4a320a72563204a0630dcb5d07a
Scott <scott@kurokoproject.com>
2010-09-28 21:18:19 -0700

    add customizations from work

--- home/.emacs.d/mecha.el
+++ home/.emacs.d/mecha.el
@@ -4,12 +4,24 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
-(setq org-agentda-files (list "~/Dropbox/Org/home.org" 
-                              "~/Dropbox/Org/projects.org"))
+(global-set-key (kbd "<f2>") 'org-agenda)
+
+(setq org-agenda-files (list "/Users/mecha/Dropbox/Org/home.org" 
+                              "/Users/mecha/Dropbox/Org/games.org"
+                              "/Users/mecha/Dropbox/Org/projects.org"))
 
 ;; FIXME: not sure how to add this to emacs load path, since this
 ;; is a MacPorts-ism
 (setq-default ispell-program-name "/opt/local/bin/aspell")
+
+;; Vimpulse setup
+;; FIXME: not sure if this is the right way: unlike Vim the first j will be captured
+;; instead of echoed.
+;; (vimpulse-imap (kbd "jj") vimpulse-exit-insert-state)
+
+;; smart-tab setup
+(require 'smart-tab)
+(global-smart-tab-mode 1)
 
 ;; look-n-feel
 (set-face-attribute 'default nil :font "Inconsolata-dz-13")
