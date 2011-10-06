e2a4a884d38109160465ca96724cf1a11dccb6c7
Scott Francis <scott@kurokoproject.com>
2011-10-06 12:08:34 -0700

    Don't load the color-theme package anymore, since Emacs 24 has deftheme

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -9,7 +9,7 @@
 
 ;; Add in your own as you wish:
 ;; starter-kit-bindings
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby color-theme yasnippet yasnippet-bundle)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby yasnippet yasnippet-bundle)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
