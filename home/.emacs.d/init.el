43ed255f539e4479c2cb08139d58bdfbfe35cfc6
Scott Francis <scott@kurokoproject.com>
2013-09-08 12:39:50 -0700

    update some autoupdated files

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -15,3 +15,15 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
       (package-install p)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
